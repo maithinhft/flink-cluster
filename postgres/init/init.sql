d047a63a1d15372e1ca0625727b357e4e3181c71
maithinhft <maithinh.lhpnd@gmail.com>
2026-08-18 14:44:37 +0700

    update: kafka connector

--- postgres/init/init.sql
+++ postgres/init/init.sql
@@ -1,4 +1,26 @@
-CREATE TABLE aggregation_definitions (
+
+DO $$
+BEGIN
+  IF NOT EXISTS (
+    SELECT FROM pg_roles
+    WHERE rolname = 'replicator'
+  ) THEN
+    CREATE ROLE replicator
+      WITH LOGIN
+      REPLICATION
+      PASSWORD 'replicatorpassword';
+  END IF;
+END
+$$;
+
+
+
+GRANT CONNECT ON DATABASE realtime_core TO replicator;
+
+\c realtime_core
+
+
+CREATE TABLE IF NOT EXISTS aggregation_definitions (
     id BIGSERIAL PRIMARY KEY,
 
     aggregation_id UUID NOT NULL,
@@ -21,10 +43,16 @@ CREATE TABLE aggregation_definitions (
 
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT uq_aggregation_version
+        UNIQUE (aggregation_id, version)
 );
 
-CREATE TABLE rule_definitions (
+ALTER TABLE aggregation_definitions
+REPLICA IDENTITY FULL;
+
+CREATE TABLE IF NOT EXISTS rule_definitions (
     id BIGSERIAL PRIMARY KEY,
 
     rule_id UUID NOT NULL,
@@ -39,5 +67,28 @@ CREATE TABLE rule_definitions (
 
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT uq_rule_version
+        UNIQUE (rule_id, version)
 );
+
+
+ALTER TABLE rule_definitions
+REPLICA IDENTITY FULL;
+
+GRANT USAGE ON SCHEMA public TO replicator;
+
+GRANT SELECT ON
+    public.aggregation_definitions,
+    public.rule_definitions
+TO replicator;
+
+GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO replicator;
+
+
+CREATE PUBLICATION realtime_publication
+FOR TABLE
+    public.aggregation_definitions,
+    public.rule_definitions;
+
---
+++ scripts/create-kafka-topics.sh
@@ -0,0 +1,24 @@
+docker exec -it kafka \
+  /opt/kafka/bin/kafka-topics.sh \
+  --create \
+  --bootstrap-server kafka:29092 \
+  --topic realtime_core.public.aggregation_definitions \
+  --partitions 6 \
+  --replication-factor 1
+
+docker exec -it kafka \
+  /opt/kafka/bin/kafka-topics.sh \
+  --create \
+  --bootstrap-server kafka:29092 \
+  --topic realtime_core.public.rule_definitions \
+  --partitions 6 \
+  --replication-factor 1
+
+docker exec -it kafka \
+  /opt/kafka/bin/kafka-topics.sh \
+  --create \
+  --if-not-exists \
+  --bootstrap-server kafka:29092 \
+  --topic __debezium-heartbeat.realtime_core \
+  --partitions 1 \
+  --replication-factor 1
