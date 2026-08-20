3b814819653f15b9c1e22555f76283fc9046e940
maithinhft <maithinh.lhpnd@gmail.com>
2026-08-20 16:52:26 +0700

    update: kafka topics

--- scripts/create-kafka-topics.sh
+++ scripts/create-kafka-topics.sh
@@ -2,14 +2,6 @@ docker exec -it kafka \
   /opt/kafka/bin/kafka-topics.sh \
   --create \
   --bootstrap-server kafka:29092 \
-  --topic realtime_core.public.aggregation_definitions \
-  --partitions 6 \
-  --replication-factor 1
-
-docker exec -it kafka \
-  /opt/kafka/bin/kafka-topics.sh \
-  --create \
-  --bootstrap-server kafka:29092 \
   --topic realtime_core.public.rule_definitions \
   --partitions 6 \
   --replication-factor 1
