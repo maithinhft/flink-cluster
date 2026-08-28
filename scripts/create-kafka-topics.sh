39708f4bb41faca1e6f474f406060b345bc51252
maithinhft <maithinh.lhpnd@gmail.com>
2026-08-28 15:31:05 +0700

    update: config

--- scripts/create-kafka-topics.sh
+++ scripts/create-kafka-topics.sh
@@ -1,44 +1,49 @@
 docker exec -it kafka \
   /opt/kafka/bin/kafka-topics.sh \
-  --create \
   --bootstrap-server kafka:29092 \
   --topic realtime_core.public.rule_definitions \
+  --create \
+  --if-not-exists \
   --partitions 6 \
   --replication-factor 1
 
 docker exec -it kafka \
   /opt/kafka/bin/kafka-topics.sh \
-  --create \
-  --if-not-exists \
   --bootstrap-server kafka:29092 \
   --topic __debezium-heartbeat.realtime_core \
+  --create \
+  --if-not-exists \
   --partitions 1 \
   --replication-factor 1
 
 docker exec kafka /opt/kafka/bin/kafka-topics.sh \
   --bootstrap-server kafka:29092 \
-  --create --if-not-exists \
   --topic events.crm \
-  --partitions 6 \
-  --replication-factor 1
-
-docker exec kafka /opt/kafka/bin/kafka-topics.sh \
-  --bootstrap-server kafka:29092 \
-  --create --if-not-exists \
-  --topic events.ecommerce \
-  --partitions 6 \
-  --replication-factor 1
-
-docker exec kafka /opt/kafka/bin/kafka-topics.sh \
-  --bootstrap-server kafka:29092 \
-  --create --if-not-exists \
-  --topic events.payment \
-  --partitions 6 \
-  --replication-factor 1
-
-docker exec kafka /opt/kafka/bin/kafka-topics.sh \
-  --bootstrap-server kafka:29092 \
   --create \
+  --if-not-exists \
+  --partitions 6 \
+  --replication-factor 1
+
+docker exec kafka /opt/kafka/bin/kafka-topics.sh \
+  --bootstrap-server kafka:29092 \
+  --topic events.ecommerce \
+  --create \
+  --if-not-exists \
+  --partitions 6 \
+  --replication-factor 1
+
+docker exec kafka /opt/kafka/bin/kafka-topics.sh \
+  --bootstrap-server kafka:29092 \
+  --topic events.payment \
+  --create \
+  --if-not-exists \
+  --partitions 6 \
+  --replication-factor 1
+
+docker exec kafka /opt/kafka/bin/kafka-topics.sh \
+  --bootstrap-server kafka:29092 \
   --topic schema_registry \
+  --create \
+  --if-not-exists \
   --partitions 6 \
   --replication-factor 1
